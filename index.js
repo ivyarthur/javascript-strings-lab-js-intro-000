b0f207fae2ef4963ef0cc096e993d3c4ccbddf96
Emma Scott <ejms236@gmail.com>
2019-09-01 11:36:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello, " + ${specialGuest} + "!";
+var greetSpecialGuest = `Hello, ${specialGuest}!`;
 
 var topic = "space";
 
 var conversation = `Let's talk about ${topic}`;
+
+
+// 1) "asfdsafasdfa" + specialGuest + "asdfafadsfasd" + 10 + "asdfasdfadsfa" + topc
+// 2) `asdfasdfasdf ${specialGuest}`
